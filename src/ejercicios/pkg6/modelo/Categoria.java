d0d21b38a81b1bd04a2cce71991435f10e38e8b9
FiorellaAilen <fiorellaailen@gmail.com>
2023-09-01 18:28:31 -0300

    Initial commit

---
+++ src/ejercicios/pkg6/modelo/Categoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios.pkg6.modelo;
+
+/**
+ *
+ * @author Fiorella Ailén
+ */
+public enum Categoria {
+    
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA
+    
+}
